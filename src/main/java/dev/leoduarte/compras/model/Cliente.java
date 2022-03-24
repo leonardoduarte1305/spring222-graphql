7f2f10bb696b66d449ef706f736dbe36bf069e79
Leonardo Duarte <leonardoduarte1305@gmail.com>
2022-03-24 18:02:06 -0300

    Criando novas entidade

---
+++ src/main/java/dev/leoduarte/compras/model/Cliente.java
@@ -0,0 +1,14 @@
+package dev.leoduarte.compras.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Cliente {
+
+	private String nome;
+	private String email;
+}
